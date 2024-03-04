49987b1dfb27d2aef6f55a34f8fefe1cb5904ea5
Harisudhan.S <93301637+Harisudhan5@users.noreply.github.com>
2024-03-04 23:09:43 +0530

    Update bounding_box.py
    commented

--- bounding_box.py
+++ bounding_box.py
@@ -1,9 +1,10 @@
 import pytesseract
 import cv2
-
+#reading the image
 image = cv2.imread("Test Images//3.png")
 img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 hImg, wImg, c = img.shape
+#getting the bounding boxes of the text using pytesseract
 boxes = pytesseract.image_to_data(img)
 bounding_boxes = []
 for x,b in enumerate(boxes.splitlines()):
@@ -11,6 +12,7 @@ for x,b in enumerate(boxes.splitlines()):
         b = b.split()
         if len(b) == 12 and b[-1] != "-1":
             print(b)
+            #displaying the text with bounding box for words
             x,y,w,h = int(b[6]),int(b[7]),int(b[8]),int(b[9])
             cv2.rectangle(img,(x,y),(w+x,h+y),(0,0,255),2)
             bounding_boxes.append([x,y,w,h])
